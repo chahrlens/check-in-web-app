1a78007a5fd760de4a6b9834b3f74a8904b67b42
Carlos Zepeda <carlos.ihernandez7@gmail.com>
2025-09-01 15:21:32 -0600

    .

--- lib/shared/constants/config.dart
+++ lib/shared/constants/config.dart
@@ -1,5 +1,5 @@
 class Config {
   static const bool isDebug = false;
-  static String endPointBaseUrl = 'http://localhost:4000/guest';
-  // static const String endPointBaseUrl = 'https://sistemas.qa.elebano.com/guest';
+  // static String endPointBaseUrl = 'http://localhost:4000/guest';
+  static const String endPointBaseUrl = 'https://sistemas.qa.elebano.com/guest';
 }
